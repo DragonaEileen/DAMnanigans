50e4507e3161464ce2f57de04cc3d53d87e62566
emiralles <emiralles@106-30.info2>
2023-10-10 13:54:57 +0200

    Dados funciona

--- src/ejerciciosswitch/Ejercicio1.java
+++ src/ejerciciosswitch/Ejercicio1.java
@@ -3,75 +3,75 @@ package ejerciciosswitch;
 import java.util.Scanner;
 
 public class Ejercicio1 {
-	
-	/*Pedir una nota entera de 0 a 10 y mostrarla de la siguiente forma: insuficiente (de 0 a 4), suficiente (5), 
-	 * bien (6), notable (7 y 8) y sobresaliente (9 y 10).
+
+	/*
+	 * Pedir una nota entera de 0 a 10 y mostrarla de la siguiente forma:
+	 * insuficiente (de 0 a 4), suficiente (5), bien (6), notable (7 y 8) y
+	 * sobresaliente (9 y 10).
 	 */
 
 	public static void main(String[] args) {
-		
-		//Variable declaration
-			//La nota a mostrar
+
+		// Variable declaration
+		// La nota a mostrar
 		int nota;
-		
-		//Scanner opening
+
+		// Scanner opening
 		Scanner sc = new Scanner(System.in);
-		
-		//Data recollection
-		System.out.println("Hoi!!!, tEMMIE here, wut's up?\r\n"
-				+ "tEMMIE haz exam and tEMMIE got a number\r\n"
-				+ "tEMMIE does not know WUT score she get \r\n"
-				+ "can u tell tEMMIE teh score????");
-			//Preguntamos la Nota
+
+		// Data recollection
+		System.out.println("Hoi!!!, tEMMIE here, wut's up?\r\n" + "tEMMIE haz exam and tEMMIE got a number\r\n"
+				+ "tEMMIE does not know WUT score she get \r\n" + "can u tell tEMMIE teh score????");
+		// Preguntamos la Nota
 		System.out.print("what number did temmie get?");
-		nota  = sc.nextInt();
-		
-		//tEMMIE Algorithm
-		switch (nota) {		//Dependiendo de la nota te dirá si has aprobado o no y con que valoración
-			case 0:
-				System.out.println("tEMMIE got an f ????");
-				break;
-			case 1:
-				System.out.println("tEMMIE got an f ????");
-				break;
-			case 2:
-				System.out.println("tEMMIE got an f ????");
-				break;
-			case 3:
-				System.out.println("tEMMIE got an f ????");
-				break;
-			case 4:
-				System.out.println("tEMMIE got an f ????");
-				break;
-			case 5:
-				System.out.println("tEMMIE got a SUFFICIENTE owo");
-				break;
-			case 6:
-				System.out.println("tEMMIE got a gud uwu");
-				break;
-			case 7:
-				System.out.println("tEMMIE got an notorious git gud");
-				break;
-			case 8:
-				System.out.println("tEMMIE got an notorious git gud");
-				break;
-			case 9:
-				System.out.println("tEMMIE haz a joke: y did teh scarecrow get a promotion?\r\n"
-						+ "cuz it was OUTSTANDING on it field");
-				break;
-			case 10:
-				System.out.println("tEMMIE haz a joke: y did teh scarecrow get a promotion?\r\n"
-						+ "cuz it was OUTSTANDING on it field");
-				break;
-			default:
-				System.out.println("tEMMIE doesn't know bout dis number");
-				break;
-	
+		nota = sc.nextInt();
+
+		// tEMMIE Algorithm
+		switch (nota) { // Dependiendo de la nota te dirá si has aprobado o no y con que valoración
+		case 0:
+			System.out.println("tEMMIE got an f ????");
+			break;
+		case 1:
+			System.out.println("tEMMIE got an f ????");
+			break;
+		case 2:
+			System.out.println("tEMMIE got an f ????");
+			break;
+		case 3:
+			System.out.println("tEMMIE got an f ????");
+			break;
+		case 4:
+			System.out.println("tEMMIE got an f ????");
+			break;
+		case 5:
+			System.out.println("tEMMIE got a SUFFICIENTE owo");
+			break;
+		case 6:
+			System.out.println("tEMMIE got a gud uwu");
+			break;
+		case 7:
+			System.out.println("tEMMIE got an notorious git gud");
+			break;
+		case 8:
+			System.out.println("tEMMIE got an notorious git gud");
+			break;
+		case 9:
+			System.out.println("tEMMIE haz a joke: y did teh scarecrow get a promotion?\r\n"
+					+ "cuz it was OUTSTANDING on it field");
+			break;
+		case 10:
+			System.out.println("tEMMIE haz a joke: y did teh scarecrow get a promotion?\r\n"
+					+ "cuz it was OUTSTANDING on it field");
+			break;
+		default:
+			System.out.println("tEMMIE doesn't know bout dis number");
+			break;
+
 		}
-		
-		//Scanner closing
+
+		// Scanner closing
 		sc.close();
-		
+
 	}
 
 }
--- src/ejerciciosswitch/Ejercicio5.java
+++ src/ejerciciosswitch/Ejercicio5.java
@@ -3,37 +3,66 @@ package ejerciciosswitch;
 import java.util.Scanner;
 
 public class Ejercicio5 {
-	
-	/*Queremos saber, una vez introducido un carnet de conducir, a qué corresponde. Sabiendo:
-      a) E: remolques.
-      b) D: autobuses.
-      c) C1-C5: camiones.
-      d) A: motocicletas.
-      e) B1-B2: automóviles.
-      f) En caso de introducir uno distinto: “Categoría no contemplada”.
-*/
+
+	/*
+	 * Queremos saber, una vez introducido un carnet de conducir, a qué corresponde.
+	 * Sabiendo: a) E: remolques. b) D: autobuses. c) C1-C5: camiones. d) A:
+	 * motocicletas. e) B1-B2: automóviles. f) En caso de introducir uno distinto:
+	 * “Categoría no contemplada”.
+	 */
 
 	public static void main(String[] args) {
 
-		//Variable Declaration
+		// Variable Declaration
 		String carnet;
-		
-		//Abrimos Scanner
+
+		// Abrimos Scanner
 		Scanner sc = new Scanner(System.in);
-		
-		//Data recollection
+
+		// Data recollection
 		System.out.println("Que me dices, que necesito un carnet para llevar a 10 spooky scary skeletons??");
-			//Carnet
+		// Carnet
 		System.out.print("Si yo tengo un carnet de: ");
 		carnet = sc.next();
-		
-		//Algorithm switch
-		
-		switch (carnet) {
-			case "E":
-				System.out.println("Con este carnet puedo llevar remolques.");
+
+		// Algorithm switch
+
+		switch (carnet) {	//Switch de decir cosas
+		case "E":
+			System.out.println("Con este carnet puedo llevar remolques.");
+			break;
+		case "D":
+			System.out.println("Con este carnet puedo llevar autobuses.");
+			break;
+		case "C1":
+			System.out.println("Con este carnet puedo llevar camiones.");
+			break;
+		case "C2":
+			System.out.println("Con este carnet puedo llevar camiones.");
+			break;
+		case "C3":
+			System.out.println("Con este carnet puedo llevar camiones.");
+			break;
+		case "C4":
+			System.out.println("Con este carnet puedo llevar camiones.");
+			break;
+		case "C5":
+			System.out.println("Con este carnet puedo llevar camiones.");
+			break;
+		case "A":
+			System.out.println("Con este carnet puedo llevar motociclotes.");
+			break;
+		case "B1":
+			System.out.println("Con este carnet puedo llevar automóviles.");
+			break;
+		case "B2":
+			System.out.println("Con este carnet puedo llevar AUOMÓVILES (escrito así yep).");
+			break;
 		}
 		
+		//Cerramos scanner
+		sc.close();
+
 	}
 
 }
