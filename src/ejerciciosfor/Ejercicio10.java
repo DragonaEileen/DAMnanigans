26ad7a3cad8b819f69947e44bee28085f70d2989
DragonaEileen <eileen.miralles@iesnervion.es>
2023-10-27 13:10:57 +0200

    Ejercicios for finalizados

--- src/ejerciciosfor/Ejercicio10.java
+++ src/ejerciciosfor/Ejercicio10.java
@@ -1,5 +1,7 @@
 package ejerciciosfor;
 
+import java.util.Scanner;
+
 public class Ejercicio10 {
 	
 	/** Realiza un programa que pida dos números enteros A y B, siendo B mayor que A. Luego 
@@ -13,8 +15,28 @@ public class Ejercicio10 {
 		int inputB;
 		
 		/* Apertura del Scanner */
+		Scanner sc = new Scanner(System.in);
 		
+		/*Syso de Información y Recogida de Datos*/
+		System.out.println("Dame dos números, A y B, siendo B mayor que A.");
+		System.out.print("A: ");
+		inputA = sc.nextInt();
 		
-	}
+		System.out.print("B: ");
+		inputB = sc.nextInt();
+		
+		/* Algoritmo */
+		/* Mediante un bucle for, siendo A el inicializador, vamos a ir sumandole 1 a A e imprimiendo 
+		 * los números dados mientras que sean menores que B, en cuyo caso contrario parará */
+		for (int i = inputA; i <= inputB; i++) {
+			
+			System.out.println(i);
+			
+		}//Fin For
+		
+		/* Cierre del Scanner */
+		sc.close();
+		
+	}//Fin main
 
 }
