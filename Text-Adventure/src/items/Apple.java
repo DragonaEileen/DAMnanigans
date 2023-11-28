592b8b3a96d394211f9a9bb197c3ff2f137ebfee
Cyber Dragons <elemento.gemini@gmail.com>
2023-11-28 22:01:38 +0100

    Don't know what i did

--- Text-Adventure/src/items/Apple.java
+++ Text-Adventure/src/items/Apple.java
@@ -1,9 +1,6 @@
 package items;
 
 public class Apple extends Consumable{
-
-	protected static String nombre = "Applebee";
-	
 	
 	public Apple(){
 		
@@ -12,9 +9,5 @@ public class Apple extends Consumable{
 		description = "The round fruit of a tree of the rose family, which typically has thin green or red skin and crisp flesh.";
 		
 	}
-	
-	public static void callApple() {
-		System.out.println(nombre);
-	}
-	
+
 }
